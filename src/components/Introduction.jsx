648be86fe081220b536df8fe469abf650b6eb966
Deepak Kumar <deepak21062000.com>
2025-09-20 01:02:36 +0530

    update content

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -24,18 +24,18 @@ export default function ChallengeSection() {
           <h1 className="text-4xl md:text-6xl font-semibold text-white max-w-7xl mx-auto drop-shadow-lg leading-tight">
             What is the{" "}
             <span className="text-red-600 font-extrabold">
-              DRIP Internship Challenge ?
+              IGNITE Internship Challenge ?
             </span>
           </h1>
           <div className="h-[4px] w-full max-w-5xl my-5 mx-auto bg-gradient-to-r from-transparent via-white to-transparent opacity-70"></div>
           <p className="text-2xl md:text-2xl text-gray-100 max-w-6xl font-semibold mx-auto drop-shadow-md leading-relaxed">
             A one-of-its-kind internship challenge built to spark your
-            creativity, innovation, and learning. Designed exclusively for <span className="text-red-600">Grade
-            11 & 12</span> students it brings you hands-on sessions with industry
-            experts where you'll dive into Al, social media, and beyond powered
-            by upGrad School of Technology.
+            creativity, innovation, and learning. Designed exclusively for{" "}
+            <span className="text-red-600">Grade 11 & 12</span> students, it
+            brings you hands-on sessions with industry experts where you’ll dive
+            into AI, social media, and beyond powered by upGrad School of
+            Technology. 
           </p>
-          
         </div>
       </section>
     </>
